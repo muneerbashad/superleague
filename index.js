843945869b8a279336bdbe7486bbf3fd595e1c1b
muneerbashad <muneerbasha.dalbhondi@gmail.com>
2022-12-24 13:00:39 +0000

    superleague

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  <div className="super-over-league-container">
+    <h1 className="super-over-heading">Super Over League</h1>
+    <div className="super-over-mini-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image-logo"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image-logo"
+      />
+    </div>
+  </div>
+  //   Write your code here.
+);
+
+ReactDOM.render(element, document.getElementById("root"));
